d0ee548f38082ea50601d5677056581703b9a35a
Leif Strand <leif@geodynamics.org>
2005-08-23 02:08:54 +0000

    [vcp] create branch 'r20050223'

---
+++ lithomop3d/Make.mm
@@ -0,0 +1,47 @@
+# -*- Makefile -*-
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                             Charles A. Williams
+#                       Rensselaer Polytechnic Institute
+#                        (C) 2003  All Rights Reserved
+#
+# <LicenseText>
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+PROJECT = lithomop
+PACKAGE = lithomop3d
+
+RECURSE_DIRS = \
+	liblithomop3d \
+	module \
+	lithomop3d \
+	applications \
+	tests
+
+OTHERS = \
+
+#--------------------------------------------------------------------------
+#
+
+all:
+	BLD_ACTION="all" $(MM) recurse
+
+clean::
+	BLD_ACTION="clean" $(MM) recurse
+
+tidy::
+	BLD_ACTION="tidy" $(MM) recurse
+
+distclean::
+	BLD_ACTION="distclean" $(MM) recurse
+
+
+# version
+# $Id: Make.mm,v 1.2 2004/08/02 21:39:42 willic3 Exp $
+
+# Generated automatically by MakeMill on Tue Mar  2 17:05:23 2004
+
+# End of file 
