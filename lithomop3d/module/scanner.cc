c19e46f32e7be67a96c75d9dcb4508b4d3bdfb40
Leif Strand <leif@geodynamics.org>
2005-08-23 02:10:21 +0000

    [vcp] create branch 'r20050223'

---
+++ lithomop3d/module/scanner.cc
@@ -0,0 +1,788 @@
+// -*- C++ -*-
+// 
+//  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// 
+//                               Charles A. Williams
+//                        Rensselaer Polytechnic Institute
+//                        (C) 2004 All Rights Reserved
+// 
+//  Copyright 2004 Rensselaer Polytechnic Institute.
+//  All worldwide rights reserved.  A license to use, copy, modify and
+//  distribute this software for non-commercial research purposes only
+//  is hereby granted, provided that this copyright notice and
+//  accompanying disclaimer is not modified or removed from the software.
+//
+//  DISCLAIMER:  The software is distributed "AS IS" without any express
+//  or implied warranty, including but not limited to, any implied
+//  warranties of merchantability or fitness for a particular purpose
+//  or any warranty of non-infringement of any current or pending patent
+//  rights.  The authors of the software make no representations about
+//  the suitability of this software for any particular purpose.  The
+//  entire risk as to the quality and performance of the software is with
+//  the user.  Should the software prove defective, the user assumes the
+//  cost of all necessary servicing, repair or correction.  In
+//  particular, neither Rensselaer Polytechnic Institute, nor the authors
+//  of the software are liable for any indirect, special, consequential,
+//  or incidental damages related to the software, to the maximum extent
+//  the law permits.
+// 
+//  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// 
+
+#include <portinfo>
+#include "journal/debug.h"
+
+#include <Python.h>
+
+#include "scanner.h"
+#include "exceptionhandler.h"
+#include "lithomop3d_externs.h"
+#include <stdio.h>
+#include <string.h>
+
+
+// Scan boundary conditions
+
+char pylithomop3d_scan_bc__doc__[] = "";
+char pylithomop3d_scan_bc__name__[] = "scan_bc";
+
+PyObject * pylithomop3d_scan_bc(PyObject *, PyObject *args)
+{
+  int f77FileInput;
+  char* displacementUnits;
+  char* velocityUnits;
+  char* forceUnits;
+  char* bcInputFile;
+
+  int ok = PyArg_ParseTuple(args, "issss:scan_bc",
+			    &f77FileInput,
+			    &displacementUnits,
+			    &velocityUnits,
+			    &forceUnits,
+			    &bcInputFile);
+
+  if (!ok) {
+    return 0;
+  }
+
+  int errorcode = 0;
+  const int maxsize = 1024;
+  char errorstring[maxsize];
+  int numberBcEntries = 0;
+
+  scan_bc_f(&numberBcEntries,
+	    &f77FileInput,
+	    displacementUnits,
+	    velocityUnits,
+	    forceUnits,
+	    bcInputFile,
+	    &errorcode,
+	    errorstring,
+	    strlen(displacementUnits),
+	    strlen(velocityUnits),
+	    strlen(forceUnits),
+	    strlen(bcInputFile),
+	    strlen(errorstring));
+    
+  if(0 != exceptionhandler(errorcode, errorstring)) {
+    return 0;
+  }
+
+  journal::debug_t debug("lithomop3d");
+  debug
+    << journal::at(__HERE__)
+    << "numberBcEntries:" << numberBcEntries
+    << journal::endl;
+
+  // return
+  Py_INCREF(Py_None);
+  return Py_BuildValue("i", numberBcEntries);
+}
+
+
+// Scan connectivities
+
+char pylithomop3d_scan_connect__doc__[] = "";
+char pylithomop3d_scan_connect__name__[] = "scan_connect";
+
+PyObject * pylithomop3d_scan_connect(PyObject *, PyObject *args)
+{
+  PyObject* pyPointerToListArrayNumberElementNodesBase;
+  PyObject* pyPointerToMaterialInfo;
+  PyObject* pyPointerToMaterialModelInfo;
+  PyObject* pyPointerToListArrayMaterialModel;
+  PyObject* pyPointerToListArrayPropertyListIndex;
+  int numberMaterials;
+  int f77FileInput;
+  char* connectivityInputFile;
+
+  int ok = PyArg_ParseTuple(args, "OOOOOiis:scan_connect",
+			    &pyPointerToListArrayNumberElementNodesBase,
+			    &pyPointerToMaterialInfo,
+			    &pyPointerToMaterialModelInfo,
+			    &pyPointerToListArrayMaterialModel,
+			    &pyPointerToListArrayPropertyListIndex,
+			    &numberMaterials,
+			    &f77FileInput,
+			    &connectivityInputFile);
+
+  if (!ok) {
+    return 0;
+  }
+
+  int* pointerToListArrayNumberElementNodesBase = (int*) PyCObject_AsVoidPtr(pyPointerToListArrayNumberElementNodesBase);
+  int* pointerToMaterialInfo = (int*) PyCObject_AsVoidPtr(pyPointerToMaterialInfo);
+  int* pointerToMaterialModelInfo = (int*) PyCObject_AsVoidPtr(pyPointerToMaterialModelInfo);
+  int* pointerToListArrayMaterialModel = (int*) PyCObject_AsVoidPtr(pyPointerToListArrayMaterialModel);
+  int* pointerToListArrayPropertyListIndex = (int*) PyCObject_AsVoidPtr(pyPointerToListArrayPropertyListIndex);
+  int numberElements = 0;
+  int connectivitySize = 0;
+  int errorcode = 0;
+  const int maxsize = 1024;
+  char errorstring[maxsize];
+
+  scan_connect_f(pointerToListArrayNumberElementNodesBase,
+		 pointerToMaterialInfo,
+		 pointerToMaterialModelInfo,
+		 pointerToListArrayMaterialModel,
+		 pointerToListArrayPropertyListIndex,
+		 &numberMaterials,
+		 &numberElements,
+		 &connectivitySize,
+		 &f77FileInput,
+		 connectivityInputFile,
+		 &errorcode,
+		 errorstring,
+		 strlen(connectivityInputFile),
+		 strlen(errorstring));
+    
+  if(0 != exceptionhandler(errorcode, errorstring)) {
+    return 0;
+  }
+
+  journal::debug_t debug("lithomop3d");
+  debug
+    << journal::at(__HERE__)
+    << "numberElements:" << numberElements
+    << journal::endl;
+
+  // return
+  Py_INCREF(Py_None);
+  return Py_BuildValue("ii", numberElements,
+		       connectivitySize);
+}
+
+
+// Read coordinates
+
+char pylithomop3d_scan_coords__doc__[] = "";
+char pylithomop3d_scan_coords__name__[] = "scan_coords";
+
+PyObject * pylithomop3d_scan_coords(PyObject *, PyObject *args)
+{
+  int f77FileInput;
+  char *coordinateUnits;
+  char *coordinateInputFile;
+
+  int ok = PyArg_ParseTuple(args, "iss:scan_coords",
+			    &f77FileInput,
+			    &coordinateUnits,
+			    &coordinateInputFile);
+
+  if (!ok) {
+    return 0;
+  }
+
+  int errorcode = 0;
+  const int maxsize = 1024;
+  char errorstring[maxsize];
+  int numberNodes = 0;
+
+  scan_coords_f(&numberNodes,
+		&f77FileInput,
+		coordinateUnits,
+		coordinateInputFile,
+		&errorcode,
+		errorstring,
+		strlen(coordinateUnits),
+		strlen(coordinateInputFile),
+		strlen(errorstring));
+    
+  if(0 != exceptionhandler(errorcode, errorstring)) {
+    return 0;
+  }
+
+  journal::debug_t debug("lithomop3d");
+  debug
+    << journal::at(__HERE__)
+    << "numberSpaceDimensions:" << numberNodes
+    << journal::endl;
+
+  // return
+  Py_INCREF(Py_None);
+  return Py_BuildValue("i", numberNodes);
+}
+
+
+// Read differential forces
+
+char pylithomop3d_scan_diff__doc__[] = "";
+char pylithomop3d_scan_diff__name__[] = "scan_diff";
+
+PyObject * pylithomop3d_scan_diff(PyObject *, PyObject *args)
+{
+  int numberSlipperyNodeEntries;
+  int f77FileInput;
+  char* differentialForceInputFile;
+
+  int ok = PyArg_ParseTuple(args, "iis:scan_diff",
+			    &numberSlipperyNodeEntries,
+			    &f77FileInput,
+			    &differentialForceInputFile);
+
+  if (!ok) {
+    return 0;
+  }
+
+  int errorcode = 0;
+  const int maxsize = 1024;
+  char errorstring[maxsize];
+  int numberDifferentialForceEntries = 0;
+
+  scan_diff_f(&numberSlipperyNodeEntries,
+	      &numberDifferentialForceEntries,
+	      &f77FileInput,
+	      differentialForceInputFile,
+	      &errorcode,
+	      errorstring,
+	      strlen(differentialForceInputFile),
+	      strlen(errorstring));
+    
+  if(0 != exceptionhandler(errorcode, errorstring)) {
+    return 0;
+  }
+
+  journal::debug_t debug("lithomop3d");
+  debug
+    << journal::at(__HERE__)
+    << "numberDifferentialForceEntries:" << numberDifferentialForceEntries
+    << journal::endl;
+
+  // return
+  Py_INCREF(Py_None);
+  return Py_BuildValue("i",numberDifferentialForceEntries);
+}
+
+
+// Read time steps at which full output is desired
+
+char pylithomop3d_scan_fuldat__doc__[] = "";
+char pylithomop3d_scan_fuldat__name__[] = "scan_fuldat";
+
+PyObject * pylithomop3d_scan_fuldat(PyObject *, PyObject *args)
+{
+  int analysisTypeInt;
+  int totalNumberTimeSteps;
+  int f77FileInput;
+  char* fullOutputInputFile;
+
+  int ok = PyArg_ParseTuple(args, "iiis:scan_fuldat",
+			    &analysisTypeInt,
+			    &totalNumberTimeSteps,
+			    &f77FileInput,
+			    &fullOutputInputFile);
+
+  if (!ok) {
+    return 0;
+  }
+
+  int errorcode = 0;
+  const int maxsize = 1024;
+  char errorstring[maxsize];
+  int numberFullOutputs = 0;
+
+  scan_fuldat_f(&analysisTypeInt,
+		&totalNumberTimeSteps,
+		&numberFullOutputs,
+		&f77FileInput,
+		fullOutputInputFile,
+		&errorcode,
+		errorstring,
+		strlen(fullOutputInputFile),
+		strlen(errorstring));
+    
+  if(0 != exceptionhandler(errorcode, errorstring)) {
+    return 0;
+  }
+
+  journal::debug_t debug("lithomop3d");
+  debug
+    << journal::at(__HERE__)
+    << "numberFullOutputs:" << numberFullOutputs
+    << journal::endl;
+
+  // return
+  Py_INCREF(Py_None);
+  return Py_BuildValue("i",numberFullOutputs);
+}
+
+
+// Read load histories
+
+char pylithomop3d_scan_hist__doc__[] = "";
+char pylithomop3d_scan_hist__name__[] = "scan_hist";
+
+PyObject * pylithomop3d_scan_hist(PyObject *, PyObject *args)
+{
+  int f77FileInput;
+  char* loadHistoryInputFile;
+
+  int ok = PyArg_ParseTuple(args, "is:scan_hist",
+			    &f77FileInput,
+			    &loadHistoryInputFile);
+
+  if (!ok) {
+    return 0;
+  }
+
+  int errorcode = 0;
+  const int maxsize = 1024;
+  char errorstring[maxsize];
+  int numberLoadHistories = 0;
+
+  scan_hist_f(&numberLoadHistories,
+	      &f77FileInput,
+	      loadHistoryInputFile,
+	      &errorcode,
+	      errorstring,
+	      strlen(loadHistoryInputFile),
+	      strlen(errorstring));
+    
+  if(0 != exceptionhandler(errorcode, errorstring)) {
+    return 0;
+  }
+
+  journal::debug_t debug("lithomop3d");
+  debug
+    << journal::at(__HERE__)
+    << "numberLoadHistories:" << numberLoadHistories
+    << journal::endl;
+
+  // return
+  Py_INCREF(Py_None);
+  return Py_BuildValue("i",numberLoadHistories);
+}
+
+
+// Read element prestresses
+
+  // char pylithomop3d_scan_prestr__doc__[] = "";
+  // char pylithomop3d_scan_prestr__name__[] = "scan_prestr";
+
+  // PyObject * pylithomop3d_scan_prestr(PyObject *, PyObject *args)
+  // {
+  //   int numberStressComponents;
+  //   int numberPrestressGaussPoints;
+  //   int numberElements;
+  //   int prestressAutoComputeInt;
+  //   int f77FileInput;
+  //   char* prestressInputFile;
+
+  //   int ok = PyArg_ParseTuple(args, "iiiiis:scan_prestr",
+  // 			    &numberStressComponents,
+  // 			    &numberPrestressGaussPoints,
+  // 			    &numberElements,
+  // 			    &prestressAutoComputeInt,
+  // 			    &f77FileInput,
+  // 			    &prestressInputFile);
+
+  //   if (!ok) {
+  //     return 0;
+  //   }
+
+  //   int errorcode = 0;
+  //   const int maxsize = 1024;
+  //   char errorstring[maxsize];
+  //   int numberPrestressEntries = 0;
+
+  //   scan_prestr_f(&numberStressComponents,
+  // 		&numberPrestressGaussPoints,
+  // 		&numberPrestressEntries,
+  // 		&numberElements,
+  // 		&prestressAutoComputeInt,
+  // 		&f77FileInput,
+  // 		&errorcode,
+  // 		prestressInputFile,strlen(prestressInputFile));
+    
+  //   if(0 != exceptionhandler(errorcode, errorstring)) {
+  //     return 0;
+  //   }
+
+  //   journal::debug_t debug("lithomop3d");
+  //   debug
+  //     << journal::at(__HERE__)
+  //     << "numberPrestressEntries:" << numberPrestressEntries
+  //     << journal::endl;
+
+  // return
+  //   Py_INCREF(Py_None);
+  //   return Py_BuildValue("i",numberPrestressEntries);
+  // }
+
+
+// Read local coordinate rotations
+
+char pylithomop3d_scan_skew__doc__[] = "";
+char pylithomop3d_scan_skew__name__[] = "scan_skew";
+
+PyObject * pylithomop3d_scan_skew(PyObject *, PyObject *args)
+{
+  int f77FileInput;
+  char* rotationUnits;
+  char* rotationInputFile;
+
+  int ok = PyArg_ParseTuple(args, "iss:scan_skew",
+			    &f77FileInput,
+			    &rotationUnits,
+			    &rotationInputFile);
+
+  if (!ok) {
+    return 0;
+  }
+
+  int errorcode = 0;
+  const int maxsize = 1024;
+  char errorstring[maxsize];
+  int numberRotationEntries = 0;
+
+  scan_skew_f(&numberRotationEntries,
+	      &f77FileInput,
+	      rotationUnits,
+	      rotationInputFile,
+	      &errorcode,
+	      errorstring,
+	      strlen(rotationUnits),
+	      strlen(rotationInputFile),
+	      strlen(errorstring));
+    
+  if(0 != exceptionhandler(errorcode, errorstring)) {
+    return 0;
+  }
+
+  journal::debug_t debug("lithomop3d");
+  debug
+    << journal::at(__HERE__)
+    << "numberRotationEntries:" << numberRotationEntries
+    << journal::endl;
+
+  // return
+  Py_INCREF(Py_None);
+  return Py_BuildValue("i",numberRotationEntries);
+}
+
+
+// Read slippery node entries
+
+char pylithomop3d_scan_slip__doc__[] = "";
+char pylithomop3d_scan_slip__name__[] = "scan_slip";
+
+PyObject * pylithomop3d_scan_slip(PyObject *, PyObject *args)
+{
+  int f77FileInput;
+  char* slipperyNodeInputFile;
+
+  int ok = PyArg_ParseTuple(args, "is:scan_slip",
+			    &f77FileInput,
+			    &slipperyNodeInputFile);
+
+  if (!ok) {
+    return 0;
+  }
+
+  int errorcode = 0;
+  const int maxsize = 1024;
+  char errorstring[maxsize];
+  int numberSlipperyNodeEntries = 0;
+
+  scan_slip_f(&numberSlipperyNodeEntries,
+	      &f77FileInput,
+	      slipperyNodeInputFile,
+	      &errorcode,
+	      errorstring,
+	      strlen(slipperyNodeInputFile),
+	      strlen(errorstring));
+    
+  if(0 != exceptionhandler(errorcode, errorstring)) {
+    return 0;
+  }
+
+  journal::debug_t debug("lithomop3d");
+  debug
+    << journal::at(__HERE__)
+    << "numberSlipperyNodeEntries:" << numberSlipperyNodeEntries
+    << journal::endl;
+
+  // return
+  Py_INCREF(Py_None);
+  return Py_BuildValue("i",numberSlipperyNodeEntries);
+}
+
+
+// Read split node entries
+
+char pylithomop3d_scan_split__doc__[] = "";
+char pylithomop3d_scan_split__name__[] = "scan_split";
+
+PyObject * pylithomop3d_scan_split(PyObject *, PyObject *args)
+{
+  int f77FileInput;
+  char* splitNodeInputFile;
+
+  int ok = PyArg_ParseTuple(args, "is:scan_split",
+			    &f77FileInput,
+			    &splitNodeInputFile);
+
+  if (!ok) {
+    return 0;
+  }
+
+  int errorcode = 0;
+  const int maxsize = 1024;
+  char errorstring[maxsize];
+  int numberSplitNodeEntries = 0;
+
+  scan_split_f(&numberSplitNodeEntries,
+	       &f77FileInput,
+	       splitNodeInputFile,
+	       &errorcode,
+	       errorstring,
+	       strlen(splitNodeInputFile),
+	       strlen(errorstring));
+    
+  if(0 != exceptionhandler(errorcode, errorstring)) {
+    return 0;
+  }
+
+  journal::debug_t debug("lithomop3d");
+  debug
+    << journal::at(__HERE__)
+    << "numberSplitNodeEntries:" << numberSplitNodeEntries
+    << journal::endl;
+
+  // return
+  Py_INCREF(Py_None);
+  return Py_BuildValue("i",numberSplitNodeEntries);
+}
+
+
+// Read time step data
+
+char pylithomop3d_scan_timdat__doc__[] = "";
+char pylithomop3d_scan_timdat__name__[] = "scan_timdat";
+
+PyObject * pylithomop3d_scan_timdat(PyObject *, PyObject *args)
+{
+  int f77FileInput;
+  char* timeUnits;
+  char* timeStepInputFile;
+
+  int ok = PyArg_ParseTuple(args, "iss:scan_timdat",
+			    &f77FileInput,
+			    &timeUnits,
+			    &timeStepInputFile);
+
+  if (!ok) {
+    return 0;
+  }
+
+  int errorcode = 0;
+  const int maxsize = 1024;
+  char errorstring[maxsize];
+  int numberTimeStepGroups = 0;
+  int totalNumberTimeSteps = 0;
+
+  scan_timdat_f(&totalNumberTimeSteps,
+		&numberTimeStepGroups,
+		&f77FileInput,
+		timeUnits,
+		timeStepInputFile,
+		&errorcode,
+		errorstring,
+		strlen(timeUnits),
+		strlen(timeStepInputFile),
+		strlen(errorstring));
+    
+  if(0 != exceptionhandler(errorcode, errorstring)) {
+    return 0;
+  }
+
+  journal::debug_t debug("lithomop3d");
+  debug
+    << journal::at(__HERE__)
+    << "totalNumberTimeSteps:" << totalNumberTimeSteps
+    << journal::endl;
+
+  // return
+  Py_INCREF(Py_None);
+  return Py_BuildValue("ii",numberTimeStepGroups,
+		       totalNumberTimeSteps);
+}
+
+
+// Read traction BC
+
+// char pylithomop3d_scan_traction__doc__[] = "";
+// char pylithomop3d_scan_traction__name__[] = "scan_traction";
+
+// PyObject * pylithomop3d_scan_traction(PyObject *, PyObject *args)
+// {
+  // char* tractionBcUnits;
+  // int f77FileInput;
+  // char* tractionInputFile;
+
+  // int ok = PyArg_ParseTuple(args, "sis:scan_traction",
+			    // &tractionBcUnits,
+			    // &f77FileInput,
+			    // &tractionInputFile);
+
+  // if (!ok) {
+    // return 0;
+  // }
+
+  // int errorcode = 0;
+  // const int maxsize = 1024;
+  // char errorstring[maxsize];
+  // int numberTractionBc = 0;
+
+  // scan_traction_f(&numberTractionBc,
+		  // &f77FileInput,
+		  // tractionBcUnits,
+		  // tractionInputFile,
+		  // &errorcode,
+		  // errorstring,
+		  // strlen(tractionBcUnits),
+		  // strlen(tractionInputFile),
+		  // strlen(errorstring));
+    
+  // if(0 != exceptionhandler(errorcode, errorstring)) {
+    // return 0;
+  // }
+
+  // journal::debug_t debug("lithomop3d");
+  // debug
+    // << journal::at(__HERE__)
+    // << "numberTractionBc:" << numberTractionBc
+    // << journal::endl;
+
+  // return
+  // Py_INCREF(Py_None);
+  // return Py_BuildValue("i",numberTractionBc);
+// }
+
+
+// Read winkler BC
+
+char pylithomop3d_scan_wink__doc__[] = "";
+char pylithomop3d_scan_wink__name__[] = "scan_wink";
+
+PyObject * pylithomop3d_scan_wink(PyObject *, PyObject *args)
+{
+  int f77FileInput;
+  char* winklerInputFile;
+
+  int ok = PyArg_ParseTuple(args, "is:scan_wink",
+			    &f77FileInput,
+			    &winklerInputFile);
+
+  if (!ok) {
+    return 0;
+  }
+
+  int errorcode = 0;
+  const int maxsize = 1024;
+  char errorstring[maxsize];
+  int numberWinklerEntries = 0;
+  int numberWinklerForces = 0;
+
+  scan_wink_f(&numberWinklerEntries,
+	      &numberWinklerForces,
+	      &f77FileInput,
+	      winklerInputFile,
+	      &errorcode,
+	      errorstring,
+	      strlen(winklerInputFile),
+	      strlen(errorstring));
+    
+  if(0 != exceptionhandler(errorcode, errorstring)) {
+    return 0;
+  }
+
+  journal::debug_t debug("lithomop3d");
+  debug
+    << journal::at(__HERE__)
+    << "numberWinklerForces:" << numberWinklerForces
+    << journal::endl;
+
+  // return
+  Py_INCREF(Py_None);
+  return Py_BuildValue("ii",numberWinklerEntries,
+	               numberWinklerForces);
+}
+
+
+// Read winkler BC for slippery nodes
+
+char pylithomop3d_scan_winkx__doc__[] = "";
+char pylithomop3d_scan_winkx__name__[] = "scan_winkx";
+
+PyObject * pylithomop3d_scan_winkx(PyObject *, PyObject *args)
+{
+  int numberSlipperyNodeEntries;
+  int f77FileInput;
+  char* slipperyWinklerInputFile;
+
+  int ok = PyArg_ParseTuple(args, "iis:scan_winkx",
+			    &numberSlipperyNodeEntries,
+			    &f77FileInput,
+			    &slipperyWinklerInputFile);
+
+  if (!ok) {
+    return 0;
+  }
+
+  int errorcode = 0;
+  const int maxsize = 1024;
+  char errorstring[maxsize];
+  int numberSlipperyWinklerEntries = 0;
+  int numberSlipperyWinklerForces = 0;
+
+  scan_winkx_f(&numberSlipperyNodeEntries,
+	       &numberSlipperyWinklerEntries,
+	       &numberSlipperyWinklerForces,
+	       &f77FileInput,
+	       slipperyWinklerInputFile,
+	       &errorcode,
+	       errorstring,
+	       strlen(slipperyWinklerInputFile),
+	       strlen(errorstring));
+    
+  if(0 != exceptionhandler(errorcode, errorstring)) {
+    return 0;
+  }
+
+  journal::debug_t debug("lithomop3d");
+  debug
+    << journal::at(__HERE__)
+    << "numberSlipperyWinklerForces:" << numberSlipperyWinklerForces
+    << journal::endl;
+
+  // return
+  Py_INCREF(Py_None);
+  return Py_BuildValue("ii",numberSlipperyWinklerEntries,
+		       numberSlipperyWinklerForces);
+}
+    
+// version
+// $Id: scanner.cc,v 1.4 2004/08/12 15:08:40 willic3 Exp $
+
+// End of file
