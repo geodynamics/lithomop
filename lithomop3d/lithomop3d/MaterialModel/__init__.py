bfc75b57ae8a99138fb5d1bc2c2560300252288d
Leif Strand <leif@geodynamics.org>
2005-08-23 02:10:27 +0000

    [vcp] create branch 'r20050223'

---
+++ lithomop3d/lithomop3d/MaterialModel/__init__.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# 
+#  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 
+#                               Michael A.G. Aivazis
+#                        California Institute of Technology
+#                        (C) 1998-2003 All Rights Reserved
+# 
+#  <LicenseText>
+# 
+#  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 
+
+# Modules to include.  This should probably include everything in this
+# directory.
+
+__all__ = ["MaterialModel",
+	   "IsotropicLinearElastic",
+	   "IsotropicLinearMaxwellViscoelastic"]
+
+def copyright():
+    return "lithomop3d pyre module: Copyright (c) 1998-2003 California Institute of Technology";
+
+
+# version
+__id__ = "$Id: __init__.py,v 1.2 2004/08/12 16:53:24 willic3 Exp $"
+
+#  End of file 
