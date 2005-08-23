c5af8ea7ef6fa7896edda0cd3e497bbccbe8dc4a
Leif Strand <leif@geodynamics.org>
2005-08-23 02:08:17 +0000

    [vcp] create branch 'r20050223'

---
+++ lithomop3d/liblithomop3d/eforce.f
@@ -0,0 +1,95 @@
+c -*- Fortran -*-
+c
+c~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+c
+c                             Charles A. Williams
+c                       Rensselaer Polytechnic Institute
+c                        (C) 2004  All Rights Reserved
+c
+c  Copyright 2004 Rensselaer Polytechnic Institute.
+c  All worldwide rights reserved.  A license to use, copy, modify and
+c  distribute this software for non-commercial research purposes only
+c  is hereby granted, provided that this copyright notice and
+c  accompanying disclaimer is not modified or removed from the software.
+c
+c  DISCLAIMER:  The software is distributed "AS IS" without any express
+c  or implied warranty, including but not limited to, any implied
+c  warranties of merchantability or fitness for a particular purpose
+c  or any warranty of non-infringement of any current or pending patent
+c  rights.  The authors of the software make no representations about
+c  the suitability of this software for any particular purpose.  The
+c  entire risk as to the quality and performance of the software is with
+c  the user.  Should the software prove defective, the user assumes the
+c  cost of all necessary servicing, repair or correction.  In
+c  particular, neither Rensselaer Polytechnic Institute, nor the authors
+c  of the software are liable for any indirect, special, consequential,
+c  or incidental damages related to the software, to the maximum extent
+c  the law permits.
+c
+c~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+c
+c
+      subroutine eforce(xl,sh,shj,det,gauss,evp,p,iel,nen,ngauss,
+     & getshape,bmatrix,ierr,errstrng)
+c
+c...this subroutine computes the effective forces at each node
+c   within an element: p=(b)t*evp, where p is the local force vector,
+c   b is the strain-displacement matrix, and evp is the stress
+c
+      include "implicit.inc"
+c
+c...  parameter definitions
+c
+      include "ndimens.inc"
+      include "nshape.inc"
+      include "rconsts.inc"
+      include "nconsts.inc"
+c
+c...  subroutine arguments
+c
+      integer iel,nen,ngauss,ierr
+      character errstrng*(*)
+      double precision xl(nsd,nen),sh(nsd+1,nenmax,ngaussmax)
+      double precision shj(nsd+1,nenmax,ngaussmax),det(ngaussmax)
+      double precision gauss(nsd+1,ngaussmax),evp(nstr,ngauss)
+      double precision p(ndof*nen)
+      external getshape,bmatrix
+c
+c...  local variables
+c
+cdebug      integer idb,jdb,kdb
+      integer l,nee
+      double precision shd(4,nenmax,ngaussmax),shbar(4,20),b(6,60),vol
+c
+cdebug      write(6,*) "Hello from eforce_f!"
+c
+      nee=ndof*nen
+      call getshape(xl,sh,shj,shd,shbar,det,gauss,vol,iel,nen,ngauss,
+     & ierr,errstrng)
+cdebug      write(6,*) "shd:"
+cdebug      do idb=1,ngauss
+cdebug        write(6,*) "det:",det(idb)
+cdebug        do jdb=1,nen
+cdebug          write(6,*) (shd(kdb,jdb,idb),kdb=1,4)
+cdebug        end do
+cdebug      end do
+      if(ierr.ne.izero) return
+c
+      do l=1,ngauss
+        call bmatrix(b,shd(1,1,l),shbar,nen)
+cdebug        do idb=1,nee
+cdebug          write(6,*) "b:",(b(jdb,idb),jdb=1,nstr)
+cdebug        end do
+        call dgemv("t",nstr,nee,det(l),b,nstr,evp(1,l),ione,one,p,ione)
+cdebug        write(6,*) "evp:",(evp(idb,l),idb=1,nstr)
+cdebug        write(6,*) "p:",(p(idb),idb=1,nee)
+      end do
+      return
+      end
+c
+c version
+c $Id: eforce.f,v 1.5 2004/08/02 21:07:51 willic3 Exp $
+c
+c Generated automatically by Fortran77Mill on Wed May 21 14:15:03 2003
+c
+c End of file 
