681116648a0de48028cfe6d3b35c74abb4df2f6e
Leif Strand <leif@geodynamics.org>
2005-08-23 02:14:34 +0000

    [vcp] create branch 'r20050223'

---
+++ lithomop3d/examples/lintet/bm1a/Make.mm
@@ -0,0 +1,26 @@
+# -*- Makefile -*-
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                             Charles A. Williams
+#                       Rensselaer Polytechnic Institute
+#                        (C) 2003  All Rights Reserved
+#
+# <LicenseText>
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+test: clean
+	lithomop3dapp.py -lm3dscan.fileRoot=bm1a
+
+clean::
+	$(RM_F) bm1a.ascii bm1a.plot bm1a*.inp
+
+
+# version
+# $Id: Make.mm,v 1.2 2004/08/25 16:58:36 willic3 Exp $
+
+# Generated automatically by MakeMill on Tue Mar  2 17:05:23 2004
+
+# End of file 
